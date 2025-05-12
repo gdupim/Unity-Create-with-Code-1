092b324c0efdbe1b800c5fa66d811299fd154a0a
Gabriel Dupim <gabrdupim@gmail.com>
2025-05-12 09:43:11 -0300

    nova classe da camera seguir o jogador + reorganização do código

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // variables
+    private float speed = 5.0f; // set vehicle speed
+    private float turnSpeed = 20f; // how much it strafes left (-) or right (+)
+    private float horizontalInput; // the key used to move horizontally
+    private float forwardInput; // the key used to move vertically
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +22,12 @@ public class PlayerController : MonoBehaviour
         // Move the vehicle forward
         // transform.Translate(0, 0, 1);
 
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // input gathering
+        horizontalInput = Input.GetAxis("Horizontal"); // assigning the horizontal axis (right and left arrow)
+        forwardInput = Input.GetAxis("Vertical"); // assigning the vertical axis (up and down arrow)
+
+        // vehicle movement
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); // move the car forward
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput); // move left (-) or right (+)
     }
 }
